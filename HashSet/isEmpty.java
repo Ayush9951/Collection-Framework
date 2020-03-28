064db537544bba193ed35ba95370c3cdd479fcc0
Ayush-Gupta <60573378+Ayush9951@users.noreply.github.com>
2020-03-28 22:02:50 +0530

    Update isEmpty.java

--- HashSet/isEmpty.java
+++ HashSet/isEmpty.java
@@ -8,16 +8,17 @@ class isEmpty
 		set.add(2);
 		set.add(3);
 		
+		System.out.println(set);	    /* [1,2,3] */
 		// size
 		System.out.println(set.size());     /* 3  */
 		
-		// isEmpty
-		System.out.println(set.isEmpty());  /* true */
+		// isEmpty:- true if set is empty otherwise false
+		System.out.println(set.isEmpty());  /* false */
 		
 		// clear
 		set.clear();
-		
-		System.out.println(set.isEmpty());  /* false */
+		System.out.println(set);	    /* [] */
+		System.out.println(set.isEmpty());  /* true */
 		
 		
 		
